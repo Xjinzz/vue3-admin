60e5b9a2d20a3d7591c45740567ac53c35c23e8e
jinzz <jinzhenzong@lenovo.com>
2021-07-23 15:47:58 +0800

    增加env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import path from "path"
 import { defineConfig, UserConfigExport, ConfigEnv } from "vite"
 import vuePlugin from "@vitejs/plugin-vue"
-export default ({ command }: ConfigEnv): UserConfigExport => {
+import { viteMockServe } from 'vite-plugin-mock';
+import { loadEnv } from 'vite'
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  const Env = loadEnv(mode, process.cwd())
+  console.log(Env)
   return {
     plugins: [vuePlugin(),
+      viteMockServe({
+        // default
+        mockPath: 'sever',
+        localEnabled: command === 'serve',
+        supportTs: true,
+        // 监听文件变动
+        watchFiles: true,
+        // 是否可以在控制台看到请求
+        logger: true
+      })
     ],
     resolve: {
       alias: {
